81a3e53d13385b614e49b9f5da0e99690537a1b7
iamvp7 <shihan.viswa@gmail.com>
2017-09-30 02:32:50 +0530

    new module

--- youtube-control/Firefox/background.js
+++ youtube-control/Firefox/background.js
@@ -1,132 +1,86 @@
-var actionID = '';
-
-chrome.runtime.onMessage.addListener(doyoutubeaction);
-
-// Listen to icon click
-browser.browserAction.onClicked.addListener(listenBrowserClick);
+    var actionID = '';
+    var actiondefined = '';
 
 
+    browser.runtime.onMessage.addListener(doyoutubeaction);
+
+    function doyoutubeaction(message) {
+
+	console.log(message);
+      actionID = message.actionid;
+	actiondefined = message.actiondefined; 
 
 
+      var querying = browser.tabs.query({
+        url: "*://*.youtube.com/*"
+      });
+      querying.then(logTabs, onError);
 
- // Get all the Links
-function listenBrowserClick(tab){
-	console.log("inside the listen browserclick");
-	var searching = browser.history.search({text: "youtube.com"});
-	searching.then(listURLs,tab.id);
-}
+    }
 
 
-// Iterate the youtube links
-function listURLs(urlObject,tabid){
+    // get all the tab url
+    function logTabs(tabs) {
 
-console.log("the tab id is:::::"+tabid);
+	console.log('action is:::::'+actiondefined);
 
-	var masterDiv = '';
-	for( urlobj of urlObject){
-		masterDiv += returnDiv(urlobj.url,urlobj.title);
+      for (let tab of tabs) {
+        // tab.url requires the `tabs` permission
+	if(actiondefined == 'controls'){
+       		 changeSong(tab.id); 
+	}else if(actiondefined == 'changevid'){
+        	browser.tabs.update(tab.id, {"url" :actionID});
+	}
+     }
+
+	if(tabs.length ==0 && actiondefined == 'changevid'){
+		browser.tabs.create( {"url" :actionID});
 	}
 
+    }
 
-console.log("we are going to send message");
+    function onError(error) {
+      console.log(`Error: ${error}`);
+    }
 
-browser.tabs.query({
-    currentWindow: true,
-    active: true
-  }).then((tabs) => {
-console.log("the tab id is ::"+tabs[0].id);
-browser.tabs.sendMessage(tabs[0].id, {data: urlObject});
-}).catch(onError);	
-//browser.tabs.sendMessage(parseInt(tabid), {data: urlObject});
+    function onExecuted(result) {
+      console.log(`We executed in tab 2`);
+    }
 
 
+    // do the operation
+    function changeSong(tabid) {
 
-}
-
-function onError(error) {
-  console.error(`Error: ${error}`);
-}
-
-function sendMessageToTabs(tabs){
-	console.log("inside sendMessage");
-browser.tabs.sendMessage(tabs[0].id, {data: urlObject});
-}
+      var codeToExecute = '';
+      codeToExecute = findCode(tabid);
 
 
-// Construt the div 
-function returnDiv(url,title){
-return '<div>'+'<a href="'+url+'">'+ title.replace(' - YouTube','')+'</a></div>';
-}
+      var executing = browser.tabs.executeScript(
+        tabid, {
+          code: codeToExecute
+        }
+      );
+      executing.then(onExecuted, onError);
+
+    }
 
 
-function getActiveTab() {
-  return browser.tabs.query({active: true, currentWindow: true});
-}
+    // Find the code executiong
+    function findCode(tabid) {
 
+      if (actionID == 'nextsong') {
+        return 'document.querySelector(".ytp-next-button.ytp-button").click()'
+      } else if (actionID == 'switchplay') {
+        return 'document.querySelector(".ytp-play-button.ytp-button").click()'
+      } else if (actionID == 'replay') {
+        return 'document.querySelector(".ytp-prev-button.ytp-button").click()'
+      } else if (actionID == 'lastsong') {
+        browser.tabs.executeScript(
+          tabid, {
+            code: 'document.querySelector(".ytp-prev-button.ytp-button").click()'
+          }
+        );
+        return 'document.querySelector(".ytp-prev-button.ytp-button").click()'
+      }
 
-
-
-
-
-// Play/pause/next song of V1
-function doyoutubeaction(message){
-actionID = message.actionid;
-
-var querying = browser.tabs.query({url: "*://*.youtube.com/*"});
-querying.then(logTabs, onError);
-
-}
-
-
-// get all the tab id and changeSong
-function logTabs(tabs) {
-  for (let tab of tabs) {
-    // tab.url requires the `tabs` permission
-	changeSong(tab.id);
-  }
-}
-
-function onError(error) {
-  console.log(`Error: ${error}`);
-}
-
-function onExecuted(result) {
-  console.log(`Exectured the script`);
-}
-
-
-// do the operation
-function changeSong(tabid){
-
-var codeToExecute = '';
-codeToExecute = findCode(tabid);
-
-
-var executing = browser.tabs.executeScript(
-  		tabid,
-  		{code: codeToExecute}
-	);
-executing.then(onExecuted, onError);
-
-}
-
-
-// Find the code executiong 
-function findCode(tabid){
-
-	if(actionID == 'nextsong'){
-        	return 'document.querySelector(".ytp-next-button.ytp-button").click()'
-	}else if(actionID == 'switchplay'){
-        	return 'document.querySelector(".ytp-play-button.ytp-button").click()'
-	}else if(actionID == 'replay'){
-        	return 'document.querySelector(".ytp-prev-button.ytp-button").click()'
-	}else if(actionID == 'lastsong'){
-        	  browser.tabs.executeScript(
-  				tabid,
-  				{code: 'document.querySelector(".ytp-prev-button.ytp-button").click()'}
-				);
-	return 'document.querySelector(".ytp-prev-button.ytp-button").click()'
-	}
-
-}
-
+    }
--- youtube-control/Firefox/popup/youtube.js
+++ youtube-control/Firefox/popup/youtube.js
@@ -1,25 +1,68 @@
+var urlList = [];
 
-chrome.runtime.onMessage.addListener(updatePopUp);
+mineHistory();
+
+function mineHistory() {
+
+  var searching = browser.history.search({
+    text: "https://www.youtube.com/watch?v=",
+    startTime: 0
+  });
+  searching.then(onGot);
+}
+
+function onGot(historyItems) {
+  for (item of historyItems) {
 
 
-function updatePopUp(request, sender,jsonVal){
-console.log("message received");
 
-console.log("the response we received is::::"+ request.data);
+    var videoId = getParameterByName('v', item.url);
+
+
+    var containsVal = urlList.includes(videoId)
+    if (!containsVal) {
+        urlList.push(videoId);
+        var urlToDisp = document.createElement('div');
+        urlToDisp.textContent = item.title;
+        urlToDisp.id = item.url;
+        urlToDisp.className = "videolink";
+	urlToDisp.setAttribute("style"," border: none;color: black;padding: 10px 20px;  text-decoration: none; display: inline-block; font-size: 14px; margin: 4px ; cursor: pointer;  max-width: inhert");
+        document.getElementById('showurl').appendChild(urlToDisp);
+    }
+
+  }
+
 
 }
 
+// Get the  video id
+function getParameterByName(name, url) {
 
-/*document.addEventListener("click", function(e) {
-  if (!e.target.classList.contains("beast")) {
-    return;
-  }
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
 
-  var name = e.target.textContent;
+document.addEventListener("click", function(e) {
+	
+console.log('inside click');   
 
 
-         chrome.runtime.sendMessage({"actionid": e.target.id});
+if (!e.target.classList.contains("beast")  && !e.target.classList.contains("videolink")) {
+     return;
+   }
+
+
+var action = '';
+	if (e.target.classList.contains("beast")){
+		action = 'controls';
+	}else if (e.target.classList.contains("videolink")){
+                action = 'changevid';
+        }
+
+
+         browser.runtime.sendMessage({"actionid": e.target.id, "actiondefined":action});
 });
-
-
-*/
