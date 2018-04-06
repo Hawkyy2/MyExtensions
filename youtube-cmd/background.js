a309d05140677f220dbe083f2c5ed4f35ddd7a85
Viswaprasath <shihan.viswa@gmail.com>
2018-04-07 03:43:51 +0530

    beaufied

--- youtube-cmd/background.js
+++ youtube-cmd/background.js
@@ -4,59 +4,59 @@ var commandType = '';
 
 browser.commands.onCommand.addListener(commandListening);
 
-function commandListening(commandName){
+function commandListening(commandName) {
 
-        if(commandName == 'next_song'){
-        	commandToExecute = 'document.querySelector(".ytp-next-button.ytp-button").click()';
-		commandType =1;
-	} else if(commandName == 'switch_case'){
-		commandToExecute = 'document.querySelector(".ytp-play-button.ytp-button").click()';
-		commandType =2;
-	}
+ if (commandName == 'next_song') {
+  commandToExecute = 'document.querySelector(".ytp-next-button.ytp-button").click()';
+  commandType = 1;
+ } else if (commandName == 'switch_case') {
+  commandToExecute = 'document.querySelector(".ytp-play-button.ytp-button").click()';
+  commandType = 2;
+ }
 
-console.log('hai');
+ console.log('hai');
 
 
-	var querying = browser.tabs.query({
-	        url: "*://*.youtube.com/*"
-      		});
-      querying.then(executeOnTab, onError);
+ var querying = browser.tabs.query({
+  url: "*://*.youtube.com/*"
+ });
+ querying.then(executeOnTab, onError);
 
 }
 
 function executeOnTab(tabs) {
-	var tabID = tabs[0].id; // getting the first tab ID alone
-console.log('tab id we going to exectue is'+tabID);
-	var executing = browser.tabs.executeScript(
-        tabID, {
-          code: commandToExecute
-        }
-      );
-      executing.then(onExecuted, onError);
+ var tabID = tabs[0].id; // getting the first tab ID alone
+ console.log('tab id we going to exectue is' + tabID);
+ var executing = browser.tabs.executeScript(
+  tabID, {
+   code: commandToExecute
+  }
+ );
+ executing.then(onExecuted, onError);
 
 }
 
 function onError(error) {
-      console.log(`Error: ${error}`);
+ console.log(`Error: ${error}`);
 }
 
 function onExecuted(result) {
-      console.log(`We executed script in the first youtube tab`);
+ console.log(`We executed script in the first youtube tab`);
 
-var titleVal = '';
+ var titleVal = '';
 
-if(commandType ==1){
-	titleVal = 'Song Changed';
-}else if(commandType ==2){
-	titleVal = 'Song Paused/Played';
+ if (commandType == 1) {
+  titleVal = 'Song Changed';
+ } else if (commandType == 2) {
+  titleVal = 'Song Paused/Played';
+ }
+
+ browser.notifications.create('Youtube', {
+  "type": "basic",
+  "iconUrl": browser.extension.getURL("icons/icon-96.png"),
+  "title": "Notification Title!",
+  "message": titleVal
+ });
+
+
 }
-
-	browser.notifications.create('Youtube', {
-    "type": "basic",
-    "iconUrl": browser.extension.getURL("icons/icon-96.png"),
-    "title": "Notification Title!",
-    "message": titleVal
-  });
-
-
-    }
